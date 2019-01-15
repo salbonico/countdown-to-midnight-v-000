7618c70581ba8e17b9294b9be02b29d6a5db2891
Steve Albonico <salbonico@gmail.com>
2019-01-15 16:49:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(secs)
-  counter = secs 
+  counter = secs
   while secs > 0
     puts "#{secs} SECOND(S)!"
     secs -= 1
   end
-  
+puts "HAPPY NEW YEAR!"
 end
